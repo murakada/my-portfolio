56f930192446d9c141c26b9ec319ea2fb8e453ed
vijaykumarmurakada <vijaykumarmurakada@gmail.com>
2025-02-02 17:54:58 +0000

    Added get in touch

--- src/app/components/GetInTouch.js
+++ src/app/components/GetInTouch.js
@@ -3,7 +3,9 @@
 export default function GetInTouch(){
        return (
         <div>
-              Vijay Kumar Murakada
+              Name :Vijay Kumar Murakada,
+              Email: vijaykumarmurakada@gmail.com,
+              Mobile: +91 8897891381
         </div>
        )
 }
